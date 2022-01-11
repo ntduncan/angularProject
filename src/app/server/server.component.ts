dfd296f367d18d60bedd755e762b0c58c0161ae5
ntduncan <nateduncan0@.com>
2022-01-10 22:03:38 -0700

    week 2 updates: added server component and registered it in app.modules.ts

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-server',
+    templateUrl: './server.component.html'
+}) //This decorator is required for components in angular
+
+export class ServerComponent {
+
+}
